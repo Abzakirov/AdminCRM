bb450282d357c2bfe6579fd5a3ac8ab7def76282
Abzakirov <abdullazokirov808@gmail.com>
2025-05-14 16:31:53 +0500

    error now

--- app/(main)/admins/[info_admin_id]/page.tsx
+++ app/(main)/admins/[info_admin_id]/page.tsx
@@ -4,20 +4,19 @@ import InfoAdmin from '@/components/InfoAdmin/InfoAdmin';
 async function getAdminData(id: string) {
   try {
     const response = await axiosInstance.get(`/staff/info/${id}`);
-    if (response.data && response.data.data) {
-      return response.data.data;
-    }
-    return null;
+    return response.data?.data || null;
   } catch (error) {
-    console.error("Error fetching admin data:", error);
+    console.error('Error fetching admin data:', error);
     return null;
   }
 }
 
-export default async function InfoAdminPage({ params }: { params: { info_admin_id: string } }) {
+export default async function InfoAdminPage({
+  params,
+}: {
+  params: { info_admin_id: string };
+}) {
   const adminData = await getAdminData(params.info_admin_id);
-  
-  return (
-    <InfoAdmin id={params?.info_admin_id} initialData={adminData} />
-  );
+
+  return <InfoAdmin id={params.info_admin_id} initialData={adminData} />;
 }
